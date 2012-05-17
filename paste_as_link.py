203390c35b2d3c56755b2d1a1e86f31b58d24ff9
Brett Terpstra <me@brettterpstra.com>
2012-05-17 13:36:34 -0500

    initial commit

---
+++ paste_as_link.py
@@ -0,0 +1,11 @@
+import sublime, sublime_plugin
+
+class PasteAsLinkCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		edit = self.view.begin_edit()
+		contents = sublime.get_clipboard()
+		self.view.replace(edit,self.view.sel()[0],"["+self.view.substr(self.view.sel()[0])+"]("+contents+")")
+		self.view.end_edit(edit)
+
+	def is_enabled(self):
+		return self.view.sel()
