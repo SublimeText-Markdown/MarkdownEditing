0c3e9a0690a947b3518220c57338c65f55a699c2
Joshua Geist <josh@jnicholasgeist.com>
2012-07-09 18:03:59 -0700

    Adding footnote functions and associated goodness.

--- gather_missing_links.py
+++ gather_missing_links.py
@@ -8,9 +8,10 @@ class GatherMissingLinkMarkersCommand(sublime_plugin.TextCommand):
         self.view.find_all("\]\[([^\]]*)\]", 0, "$1", markers)
         self.view.find_all("\[([^\]]*)\]\[\]", 0, "$1", markers)
         missinglinks = [link for link in set(markers) if not self.view.find_all("\n\s*\[%s\]:" % link)]
-        self.view.insert(edit, self.view.size(), "\n\n")
-        for link in missinglinks:
-            self.view.insert(edit, self.view.size(), '\n [%s]: ' % link)
+        if len(missinglinks):
+            self.view.insert(edit, self.view.size(), "\n")
+            for link in missinglinks:
+                self.view.insert(edit, self.view.size(), '\n [%s]: ' % link)
         self.view.end_edit(edit)
 
     def is_enabled(self):
