930d3cd9790230d377ffb8178dc951554031ee20
Brett Terpstra <me@brettterpstra.com>
2012-08-22 16:07:08 -0500

    new footnotes plugins

---
+++ gather_missing_links.py
@@ -0,0 +1,18 @@
+import sublime_plugin
+
+
+class GatherMissingLinkMarkersCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        edit = self.view.begin_edit()
+        markers = []
+        self.view.find_all("\]\[([^\]]+)\]", 0, "$1", markers)
+        self.view.find_all("\[([^\]]*)\]\[\]", 0, "$1", markers)
+        missinglinks = [link for link in set(markers) if not self.view.find_all("\n\s*\[%s\]:" % link)]
+        if len(missinglinks):
+            self.view.insert(edit, self.view.size(), "\n")
+            for link in missinglinks:
+                self.view.insert(edit, self.view.size(), '\n [%s]: ' % link)
+        self.view.end_edit(edit)
+
+    def is_enabled(self):
+        return self.view.sel()
