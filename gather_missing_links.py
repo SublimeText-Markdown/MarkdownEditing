78f33b7198d38ffa0b58fc30a597cf6d1ce7bdec
Ali Ayas <dev@maliayas.com>
2013-11-25 21:06:12 +0200

    Make "gather missing links" feature more intelligent

--- gather_missing_links.py
+++ gather_missing_links.py
@@ -8,9 +8,16 @@ class GatherMissingLinkMarkersCommand(sublime_plugin.TextCommand):
         self.view.find_all("\[([^\]]*)\]\[\]", 0, "$1", markers)
         missinglinks = [link for link in set(markers) if not self.view.find_all("\n\s*\[%s\]:" % link)]
         if len(missinglinks):
-            self.view.insert(edit, self.view.size(), "\n")
+            # Remove all whitespace at the end of the file
+            whitespace_at_end = self.view.find(r'\s*\z', 0)
+            self.view.replace(edit, whitespace_at_end, "\n")
+
+            # If there is not already a reference list at the and, insert a new line at the end
+            if not self.view.find(r'\n\s*\[[^\]]*\]:.*\s*\z', 0):
+                self.view.insert(edit, self.view.size(), "\n")
+
             for link in missinglinks:
-                self.view.insert(edit, self.view.size(), '\n [%s]: ' % link)
+                self.view.insert(edit, self.view.size(), '[%s]: \n' % link)
 
     def is_enabled(self):
         return True
