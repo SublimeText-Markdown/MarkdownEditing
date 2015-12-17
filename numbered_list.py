35de43dab7601f96a83be629414776b9b225c50c
Felix Hao <felixhao28@gmail.com>
2015-12-17 09:55:25 +0800

    Increment link reference definition
    related #119

--- numbered_list.py
+++ numbered_list.py
@@ -17,3 +17,17 @@ class NumberListCommand(sublime_plugin.TextCommand):
 
 	def is_enabled(self):
 		return bool(self.view.score_selector(self.view.sel()[0].a, "text.html.markdown"))
+
+class NumberListReferenceCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		view = self.view
+		sel = view.sel()[0]
+		text = view.substr(view.full_line(sel))
+		num = re.search('\d', text).start()
+		dot = text.find("]")
+		if num == 0:
+			view.erase(edit, sel)
+			view.insert(edit, sel.begin(), "\n%d]: " % (int(text[:dot]) + 1,))
+		else:
+			view.erase(edit, sel)
+			view.insert(edit, sel.begin(), "\n%s%d]: " % (text[:num], int(text[num:dot]) + 1))
