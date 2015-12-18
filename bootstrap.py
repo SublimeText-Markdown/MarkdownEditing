725c340e076fc58929b7a2cd3a517cc4648ca716
Felix Hao <felixhao28@gmail.com>
2015-12-18 09:17:30 +0800

    re-enable default Markdown package upon uninstallation
    fix #284
    
    code inspired by https://packagecontrol.io/docs/events

--- bootstrap.py
+++ bootstrap.py
@@ -1,15 +1,42 @@
-import sublime
+import sys
 
+package_name = 'MarkdownEditing'
 
-def plugin_loaded():
-    # Native package causes some conflicts.
-    disable_native_markdown_package()
+def get_ingored_packages():
+    settings = sublime.load_settings('Preferences.sublime-settings')
+    return settings.get('ignored_packages', [])
+
+def save_ingored_packages(ignored_packages):
+    settings.set('ignored_packages', ignored_packages)
+    sublime.save_settings('Preferences.sublime-settings')
 
 def disable_native_markdown_package():
-    settings = sublime.load_settings('Preferences.sublime-settings')
-    ignored_packages = settings.get('ignored_packages', [])
-
+    ignored_packages = get_ingored_packages()
     if 'Markdown' not in ignored_packages:
         ignored_packages.append('Markdown')
-        settings.set('ignored_packages', ignored_packages)
-        sublime.save_settings('Preferences.sublime-settings')
+        save_ingored_packages(ignored_packages)
+
+def enable_native_markdown_packageb():
+    ignored_packages = get_ingored_packages()
+    if 'Markdown' in ignored_packages:
+        ignored_packages.remove('Markdown')
+        save_ingored_packages(ignored_packages)
+
+def plugin_loaded():
+    from package_control import events
+
+    if events.install(package_name):
+        # Native package causes some conflicts.
+        disable_native_markdown_package()
+
+def plugin_unloaded():
+    from package_control import events
+
+    if events.remove(package_name):
+        # Native package causes some conflicts.
+        enable_native_markdown_package()
+
+# Compat with ST2
+if sys.version_info < (3,):
+    plugin_loaded()
+    unload_handler = plugin_unloaded
