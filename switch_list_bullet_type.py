839903aaeaff4c3f7a47960e106c0a6686573e69
Felix Hao <felixhao28@gmail.com>
2017-03-01 11:02:23 +0800

    Fix Switch List Bullet Type bug on ordered lists
    fix #420

--- switch_list_bullet_type.py
+++ switch_list_bullet_type.py
@@ -18,7 +18,9 @@ class SwitchListBulletTypeCommand(MDETextCommand):
             for line in lines:
                 line_content = self.view.substr(line)
                 # print(line_content)
-                m = re.match(r"^(\s*(?:>\s*)?)[*+\-](\s+.*)$", line_content)
+                m = re.match(r"^(\s*(?:>\s*)?)[" +
+                             ''.join(re.escape(i) for i in unordered_bullets) +
+                             r"](\s+.*)$", line_content)
                 if m:
                     # Transform the bullet to numbered bullet type
                     new_line = m.group(1) + str(number) + "." + m.group(2)
@@ -28,9 +30,7 @@ class SwitchListBulletTypeCommand(MDETextCommand):
                     # Insert the new item
                     todo.append([line, new_line])
                 else:
-                    m = re.match(r"^(\s*(?:>\s*)?)[" +
-                                 ''.join(re.escape(i) for i in unordered_bullets) +
-                                 r"](\s+.*)$", line_content)
+                    m = re.match(r"^(\s*(?:>\s*)?)[0-9]+\.(\s+.*)$", line_content)
                     if m:
                         # Transform the bullet to unnumbered bullet type
                         new_line = m.group(1) + unordered_bullets[0] + m.group(2)
