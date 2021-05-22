3c46766abb88e8a99cc6e55278fbc0d03313429c
DeathAxe <deathaxe82@googlemail.com>
2021-05-22 14:24:24 +0200

    Plugins: Tweak Goto Headings

--- plugins/headings/goto.py
+++ plugins/headings/goto.py
@@ -7,39 +7,39 @@ from ..view import MdeTextCommand
 class MdeGotoNextHeadingCommand(MdeTextCommand):
     def run(self, edit, same_level=True):
         view = self.view
+        headings = tuple(all_headings(view))
         new_sel = []
         for sel in view.sel():
             section_level = 0
-            found = False
-            for (title_begin, title_end, level) in all_headings(view):
-                if title_begin <= sel.a:
+            for title_begin, title_end, level in headings:
+                if title_begin <= sel.begin():
                     section_level = level
                 elif not same_level or section_level >= level:
-                    found = True
+                    new_sel.append(sublime.Region(title_begin, title_end))
                     break
-            if found:
-                new_sel.append(sublime.Region(title_begin, title_end))
-        if len(new_sel) == 0:
+
+        if not new_sel:
             sublime.status_message("No heading can be found")
-        else:
-            view.sel().clear()
-            for region in new_sel:
-                view.sel().add(region)
-                view.show(region)
+            return
+
+        view.sel().clear()
+        view.sel().add_all(new_sel)
+        view.show(new_sel[-1])
 
 
 class MdeGotoPreviousHeadingCommand(MdeTextCommand):
     def run(self, edit, same_level=True):
         view = self.view
+        headings = tuple(all_headings(view))
         new_sel = []
         max_level = 0
         last_level = 0
         found = False
         for sel in view.sel():
             prev = {}
-            for (title_begin, title_end, level) in all_headings(view):
+            for title_begin, title_end, level in headings:
                 max_level = max(max_level, level)
-                if title_end < sel.a:
+                if title_end < sel.begin():
                     for lvl in range(level, max_level + 1):
                         prev[lvl] = (title_begin, title_end)
                     last_level = level
@@ -57,10 +57,11 @@ class MdeGotoPreviousHeadingCommand(MdeTextCommand):
                         new_sel.append(sublime.Region(prev[last_level][0], prev[last_level][1]))
             elif max_level > 0:
                 new_sel.append(sublime.Region(title_begin, title_end))
-        if len(new_sel) == 0:
+
+        if not new_sel:
             sublime.status_message("No heading can be found")
-        else:
-            view.sel().clear()
-            for region in new_sel:
-                view.sel().add(region)
-                view.show(region)
+            return
+
+        view.sel().clear()
+        view.sel().add_all(new_sel)
+        view.show(new_sel[-1])
