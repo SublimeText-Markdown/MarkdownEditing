7a15cbff151c4b42cdd94960810f3c04342ee9c0
DeathAxe <deathaxe82@googlemail.com>
2021-10-04 21:09:10 +0200

    Add simple release script

--- make.cmd
+++ make.cmd
@@ -11,6 +11,8 @@ if /i "%1" == "docs" goto DOCS
 if /i "%1" == "deploy-docs" goto DEPLOY-DOCS
 if /i "%1" == "serve" goto SERVE
 if /i "%1" == "lint" goto LINT
+if /i "%1" == "release" goto RELEASE
+if /i "%1" == "branch" goto BRANCH
 goto :usage
 
 :INIT
@@ -43,6 +45,48 @@ goto :usage
     flake8
     goto :eof
 
+:BRANCH
+    if "%2"== "" goto :usage
+    git checkout -b %2
+    goto :eof
+
+:RELEASE
+    if "%2"== "" goto :usage
+    if not exist "messages/release-%2.md" (
+        echo Missing release-%2.md
+        exit /b 1
+    )
+    git checkout st3176 && git merge %2 --no-ff
+    if not errorlevel 0 (
+        echo Unable to merge %2 into st3176!
+        exit /b 1
+    )
+    git checkout master && git merge %2 --no-ff
+    if not errorlevel 0 (
+        echo Unable to merge %2 into master!
+        exit /b 1
+    )
+    echo Hit any key to push branches!
+    pause
+    call git push origin st3176
+    if not errorlevel 0 (
+        echo Failed to push st3176!
+        exit /b 1
+    )
+    call git push origin master
+    if not errorlevel 0 (
+        echo Failed to push master!
+        exit /b 1
+    )
+    echo Hit any key to publish release!
+    pause
+    : create release for ST3
+    gh release create --target st3176 -t "MarkdownEditing %2 (ST3176+)" -F "messages/release-%2.md" "3176-%2"
+    : create release for ST3
+    gh release create --target master -t "MarkdownEditing %2 (ST4107+)" -F "messages/release-%2.md" "4107-%2"
+    git fetch
+    goto :eof
+
 :VENV
     call .venv\scripts\activate.bat
     if "%VIRTUAL_ENV%" neq ".venv" exit /b 1
@@ -51,11 +95,13 @@ goto :usage
 :USAGE
     echo USAGE:
     echo.
-    echo   make ^[init^|docs^|gh-pages^|serve^|lint^]
+    echo   make ^[init^|docs^|gh-pages^|serve^|lint^|branch^|release^]
     echo.
-    echo   init        -- setup .venv and install requirements.
-    echo   docs        -- build documentation
-    echo   deploy-docs -- build documentation and publish on Github Pages
-    echo   serve       -- build documentation and serve via development server
-    echo   lint        -- run black, flake8 and pytest
+    echo   init          -- setup .venv and install requirements.
+    echo   docs          -- build documentation
+    echo   deploy-docs   -- build documentation and publish on Github Pages
+    echo   serve         -- build documentation and serve via development server
+    echo   lint          -- run black, flake8 and pytest
+    echo   branch 3.0.0  -- prepare version branch
+    echo   release 3.0.0 -- make a release
     goto :eof
